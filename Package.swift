79a62fa2d504d4cfedb9d5d79aff52b3358d599d
Matt Greenfield <matt@bigpaua.com>
2020-03-28 15:04:13 +0700

    attempting to add SwiftPM support

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftNotes",
+    products: [
+        .library(name: "SwiftNotes", targets: ["SwiftNotes"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "SwiftNotes", dependencies: [])
+    ]
+)
